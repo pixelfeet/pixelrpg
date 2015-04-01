5c13cafbbc34e61b6e8aea4c4c908433943a8ecd
Joshua Jackson <joshuamjaxon@gmail.com>
2015-04-01 14:22:40 -0500

    Began work on Panel class.

--- pixelrpg/PixelRPG/Menu/Panel.cs
+++ pixelrpg/PixelRPG/Menu/Panel.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PixelRPG.Menu
 {
     class Panel
     {
+        private Rectangle _dimensions;
+        private Texture2D _texture;
+
+        public Rectangle dimensions
+        {
+            get { return _dimensions; }
+            set { _dimensions = value; }
+        }
+
+        public Panel(Rectangle dimensions)
+        {
+            this.dimensions = dimensions;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _dimensions, Color.White);
+        }
     }
 }
